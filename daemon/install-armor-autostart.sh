98f8947451dabfa8c1c239bf9793a8a729a96062
sentry <sentry@robomaster.tongji.edu.cn>
2022-03-08 21:16:05 +0800

    Fix CMake

--- daemon/install-armor-autostart.sh
+++ daemon/install-armor-autostart.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/sh
-apt intall xvfb # FIXME: 直接不开启GUI,而不是用xvfb-run
+apt install xvfb # FIXME: 直接不开启GUI,而不是用xvfb-run
 mkdir -p /usr/local/lib/systemd/system
 cp daemon/armor-autostart.service /usr/local/lib/systemd/system
 systemctl daemon-reload
